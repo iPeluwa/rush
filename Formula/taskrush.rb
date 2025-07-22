ce1628d4f085ff654c149a2005bcd44938f97f87
GitHub Action <action@github.com>
2025-07-22 09:00:33 +0000

    🔄 Sync all versions to v0.2.4

--- Formula/taskrush.rb
+++ Formula/taskrush.rb
@@ -1,7 +1,7 @@
 class Taskrush < Formula
   desc "A modern task runner with parallel execution and intelligent caching"
   homepage "https://github.com/iPeluwa/rush"
-  url "https://github.com/iPeluwa/rush/releases/download/v0.2.3/rush-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/iPeluwa/rush/releases/download/v0.2.4/rush-x86_64-apple-darwin.tar.gz"
   sha256 ""  # Will be filled automatically by the action
   license "MIT"
 
